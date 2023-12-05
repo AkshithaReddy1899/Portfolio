af279ac2fb50f2537ecc2715ca7b07fcaedf7d76
Akshitha Reddy <akshithareddy1899@gmail.com>
2023-12-05 08:28:35 -0800

    Update projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,21 +5,41 @@ import data from '../assets/data';
 const Projects = () => (
   <div className="text-center mt-24 lg:mt-12 md:pt-20 my-10">
     <h1 className="text-2xl text-gray-500">Projects</h1>
-    <div className="mb-20 mt-12 flex flex-wrap flex-col md:flex-row justify-center items-center">
+    <div className="mb-20 mt-12 flex flex-wrap flex-col justify-center items-center md:hidden">
       {data.map((item) => (
-        <div className={`w-72 h-auto md:w-80 md:h-auto lg:w-96 lg:h-96 m-6 rounded p-4 bg-green-900 bg-[url('${item.img}')]`} key={item.id}>
-          <Zoom left>
-            <img src={item.img} alt={item.title} className="w-64 h-auto md:w-72 md:h-auto lg:w-80 lg:h-auto mx-auto rounded" />
+        <div className="p-4">
+        <div className="w-72 h-auto md:w-80 md:h-auto lg:w-96 lg:h-96 m-6 rounded bg-green-900 backdrop-saturate-200" style={{ backgroundImage: `url(${item.img}) ` }} key={item.id}>
+          <div className= "p-4 backdrop-saturate-200 bg-black/70">
+          <Zoom >
             <div className="m-2 p-2">
-              {/* <h2>{item.img}</h2> */}
-              <h1 className="text-left text-2xl px-4">{item.title}</h1>
-              <p className="text-xs text-left m-2">{item.description}</p>
+              <h1 className="text-left text-2xl px-4 text-slate-200">{item.title}</h1>
+              <p className="text-xs text-left m-2 text-slate-300">{item.description}</p>
               <div className="btn text-left">
                 <a href={item.repo} target="_blank" rel="noreferrer" alt="github" className="m-2 p-2 bg-emerald-600 text-white font-bold leading-normal uppercase rounded shadow-md hover:bg-emerald-700 hover:shadow-lg focus:bg-emerald-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-emerald-800 active:shadow-lg transition duration-150 ease-in-out items-center text-sm">Github</a>
-                {/* <a href={item.link} target="_blank" rel="noreferrer" alt="demo" className="m-2 p-2 bg-emerald-600 text-white font-bold leading-normal uppercase rounded shadow-md hover:bg-emerald-700 hover:shadow-lg focus:bg-emerald-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-emerald-800 active:shadow-lg transition duration-150 ease-in-out items-center text-sm">Live Demo</a> */}
               </div>
             </div>
           </Zoom>
+          </div>
+          </div>
+        </div>
+      ))}
+    </div>
+    <div className="mb-20 mt-12 hidden md:flex flex-col justify-center items-center">
+      {data.map((item) => (
+        <div className="p-4">
+        <div className="w-72 h-auto md:w-80 md:h-auto lg:w-96 lg:h-96 m-6 rounded bg-green-900 backdrop-saturate-200" style={{ backgroundImage: `url(${item.img}) ` }} key={item.id}>
+          <div className= "p-4 backdrop-saturate-200 bg-black/70">
+          <Zoom >
+            <div className="m-2 p-2">
+              <h1 className="text-left text-2xl px-4 text-slate-200">{item.title}</h1>
+              <p className="text-xs text-left m-2 text-slate-300">{item.description}</p>
+              <div className="btn text-left">
+                <a href={item.repo} target="_blank" rel="noreferrer" alt="github" className="m-2 p-2 bg-emerald-600 text-white font-bold leading-normal uppercase rounded shadow-md hover:bg-emerald-700 hover:shadow-lg focus:bg-emerald-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-emerald-800 active:shadow-lg transition duration-150 ease-in-out items-center text-sm">Github</a>
+              </div>
+            </div>
+          </Zoom>
+          </div>
+          </div>
         </div>
       ))}
     </div>

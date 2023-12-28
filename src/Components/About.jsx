a583bccd8d6fbfc1f749af9c0f46cb0aaccc28ef
Akshitha Reddy <akshithareddy1899@gmail.com>
2023-12-28 08:40:00 -0800

    Update resume

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -14,11 +14,11 @@ const About = () => (
       </div>
       <div className="text-left text-gray-300 md:p-8 p-4 lg:w-1/2 lg:p-6">
         <Fade>
-          <p className="py-4">Hello! I am Akshitha, and I enjoy websites. I have a passion for creating the most beautiful and functional products. My programming skills are at the forefront of my work, with a focus on design and usability.
+          <p className="py-4">Hello! I am Akshitha, and I enjoy building websites. I have a passion for creating the most beautiful and functional products. My programming skills are at the forefront of my work, with a focus on design and usability.
           </p>
           <div className="flex space-x-2 justify-center">
             <div>
-              <a href={akshithaReddyResume} alt="resume" target="_blank" className="px-6 pt-2.5 pb-2 bg-green-600 text-white font-bold leading-normal uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out flex align-center justify-center items-center md:text-1.5xl" rel="noreferrer" download>
+              <a href={akshithaReddyResume} alt="resume" target="_blank" className="p-3 text-lg bg-green-600 text-white font-bold leading-normal uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg focus:bg-green-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-800 active:shadow-lg transition duration-150 ease-in-out flex align-center justify-center items-center md:text-1.5xl" rel="noreferrer" download>
                 Download Resume
               </a>
             </div>
